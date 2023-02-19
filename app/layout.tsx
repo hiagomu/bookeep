cf010e2611798a2581eaf87779c9fdd41b153eb7
hiagomu <hiagomuuu@gmail.com>
2023-02-18 22:26:55 -0300

    feat: add next/font/google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import './globals.css'
 
+import { Nunito } from '@next/font/google'
+
+const nunito = Nunito({ subsets: ['latin']})
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en" className={nunito.className}>
       <head />
       <body>{children}</body>
     </html>
