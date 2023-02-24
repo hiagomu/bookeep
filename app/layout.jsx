9ea3bcab86deffdd3e632c56bbc1ae04d55c3f48
Hiago Murilo <HiagoMuuu@gmail.com>
2023-02-23 21:03:42 -0300

    fix: change lang to pt-BR

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children
 }) {
   return (
-    <html lang="en" className={nunito.className}>
+    <html lang="pt-BR" className={nunito.className}>
       <head />
       <body className='flex flex-col items-center dark:bg-primaryDarkColor'>
         <AuthContext>
