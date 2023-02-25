0b9bcc807600a1a34f3c94accf117be3bd2fb6dd
hiagomu <hiagomuuu@gmail.com>
2023-02-25 17:57:07 -0300

    fix: pwa icons on mobile devices

--- app/head.tsx
+++ app/head.tsx
@@ -7,6 +7,19 @@ export default function Head() {
       <link rel="shortcut icon" href="/assets/logo.png" />
       <link rel="manifest" href="/manifest.json" />
       <meta name="theme-color" content="#7856FF" />
+      <meta name="application-name" content="BooKeep" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="BooKeep" />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+      <meta name="msapplication-TileColor" content="#2B5797" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="theme-color" content="#FFFFFF" />
+      <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
     </>
   )
 }
