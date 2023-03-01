9898cd925f32a4f250b1ca15b3f87d54c8cf2497
Hiago Murilo <hiagomuuu@gmail.com>
2023-03-01 04:00:04 -0300

    fix: remove typescript string()

--- pages/api/twitter/twitterConfig.ts
+++ pages/api/twitter/twitterConfig.ts
@@ -1,10 +1,10 @@
 import Twitter from "twitter-lite";
 
 const client = new Twitter({
-    consumer_key: String(process.env.TWITTER_CONSUMER_KEY),
-    consumer_secret: String(process.env.TWITTER_CONSUMER_KEY_SECRET),
-    access_token_key: String(process.env.TWITTER_ACCESS_TOKEN_KEY),
-    access_token_secret: String(process.env.TWITTER_ACCESS_TOKEN_SECRET)
+    consumer_key: process.env.TWITTER_CONSUMER_KEY || "",
+    consumer_secret: process.env.TWITTER_CONSUMER_KEY_SECRET || "",
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   });
 
 export default client
