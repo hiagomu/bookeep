073867ad2fd0cf54e773985e362974bc34a914f5
Hiago Murilo <hiagomuuu@gmail.com>
2023-03-05 00:56:01 -0300

    fix: error api call

--- pages/api/posts/[details].js
+++ pages/api/posts/[details].js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
 
             return res.status(200).json(data)
         } catch (err) {
-            res.status(403).json({ err: "Erro ocorreu ao buscar o post"})
+            res.status(403).json({ err })
         }
     }
 }
