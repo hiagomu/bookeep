d3fa9d465a34cf640cc936ad5aaddacb87827d34
Hiago Murilo <hiagomuuu@gmail.com>
2023-03-29 01:27:47 -0300

    feat: add login modal

--- app/components/Login/Login.tsx
+++ app/components/Login/Login.tsx
@@ -15,7 +15,23 @@ export const Login = ({
             isOpen={isOpen}
             setIsOpen={setIsOpen}
         >
-            <button className="text-black" onClick={() => signIn()}>Login</button>
+            <div className="px-10 py-6 bg-white dark:bg-secondaryDarkColor rounded-lg flex flex-col justify-center items-center max-sm:px-6 max-sm:py-4">
+                <span className="text-black dark:text-white mb-3 font-poppins text-xl font-semibold max-sm:text-lg">Deseja fazer login?</span>
+                <div className="flex gap-5">
+                    <button
+                        className="bg-primaryColor hover:bg-primaryHoverColor text-white py-2 px-4 rounded-lg font-poppins text-lg font-semibold outline-none max-sm:text-base max-sm:px-2 max-sm:py-1.5"
+                        onClick={() => signIn()}
+                    >
+                        Login
+                    </button>
+                    <button
+                        className="bg-red-500 hover:opacity-70 text-white py-2 px-4 rounded-lg font-poppins text-lg font-semibold outline-none max-sm:text-base max-sm:px-2 max-sm:py-1.5"
+                        onClick={() => setIsOpen(false)}
+                    >
+                        Cancelar
+                    </button>
+                </div>
+            </div>
         </Modal>
     )
 }
--- app/post/[slug]/components/CommentSection.tsx
+++ app/post/[slug]/components/CommentSection.tsx
@@ -1,6 +1,7 @@
 import { Comment } from "@/app/@types"
 import Image from "next/image"
 import { MdVerified as VerifiedIcon } from 'react-icons/md'
+import Link from "next/link"
 
 interface ICommentSection {
     comments: Comment[]
@@ -13,18 +14,25 @@ const CommentSection = ({ comments }: ICommentSection) => {
             {
                 comments?.map(comment => {
                     return <div className='flex py-2'>
-                        <Image
-                            alt="Imagem de perfil do usuário"
-                            src={comment.user.image}
-                            className="mr-3 rounded-full w-9 h-9 max-md:w-7 max-md:h-7"
-                            width={36}
-                            height={36}
-                        />
+                        <Link
+                            href={`/profile/${comment.user.id}`}
+                        >
+                            <Image
+                                alt="Imagem de perfil do usuário"
+                                src={comment.user.image}
+                                className="mr-3 rounded-full w-9 h-9 max-md:w-7 max-md:h-7"
+                                width={36}
+                                height={36}
+                            />
+                        </Link>
                         <div className='flex flex-col'>
-                            <span className='text-primaryColor font-bold flex items-center max-md:text-sm'>
+                            <Link
+                                href={`/profile/${comment.user.id}`}
+                                className='text-primaryColor font-bold flex items-center max-md:text-sm'
+                            >
                                 {comment.user.name}
                                 { true && <VerifiedIcon className="text-primaryColor max-sm:text-xs ml-1"/> }
-                            </span>
+                            </Link>
                             <p className='text-black dark:text-white max-md:text-sm break-words w-[46rem] max-xl:w-[35rem] max-md:w-[27.5rem] max-sm:w-[13.5rem]'>{comment.message}</p>
                         </div>
                     </div>
